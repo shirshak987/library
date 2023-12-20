f0c43f3a5f71000630b0b1b83ad74fbdfd24e1d5
shirshak987 <153817106+shirshak987@users.noreply.github.com>
2023-12-20 17:05:01 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -15,42 +15,52 @@
 	<body>
 		<form class="cd-form" method="POST" action="#">
 		
-		<center><legend>Librarian Login</legend></center>
-
+		<center><legend>Member Login</legend></center>
+			
 			<div class="error-message" id="error-message">
 				<p id="error"></p>
 			</div>
 			
 			<div class="icon">
-				<input class="l-user" type="text" name="l_user" placeholder="Username" required />
+				<input class="m-user" type="text" name="m_user" placeholder="Username" required />
 			</div>
 			
 			<div class="icon">
-				<input class="l-pass" type="password" name="l_pass" placeholder="Password" required />
+				<input class="m-pass" type="password" name="m_pass" placeholder="Password" required />
 			</div>
 			
-			<input type="submit" value="Login" name="l_login"/>
+			<input type="submit" value="Login" name="m_login" />
+			
+			<br /><br /><br /><br />
+			
+			<p align="center">Don't have an account?&nbsp;<a href="register.php" style="text-decoration:none; color:red;">Register Now!</a>
 
-			
-			
+			<p align="center"><a href="../index.php" style="text-decoration:none;">Go Back</a>
 		</form>
-		<p align="center"><a href="../index.php" style="text-decoration:none;">Go Back</a>
 	</body>
 	
 	<?php
-		if(isset($_POST['l_login']))
+		if(isset($_POST['m_login']))
 		{
-			$query = $con->prepare("SELECT id FROM librarian WHERE username = ? AND password = ?;");
-			$query->bind_param("ss", $_POST['l_user'], sha1($_POST['l_pass']));
+			$query = $con->prepare("SELECT id, balance FROM member WHERE username = ? AND password = ?;");
+			$query->bind_param("ss", $_POST['m_user'], sha1($_POST['m_pass']));
 			$query->execute();
-			if(mysqli_num_rows($query->get_result()) != 1)
-				echo error_without_field("Invalid username/password combination");
-			else
+			$result = $query->get_result();
+			
+			if(mysqli_num_rows($result) != 1)
+				echo error_without_field("Invalid details or Account has not been activated yet!");
+			else 
 			{
-				$_SESSION['type'] = "librarian";
-				// $_SESSION['id'] = mysqli_fetch_array($result)[0];
-				$_SESSION['username'] = $_POST['l_user'];
-				header('Location: home.php');
+				$resultRow = mysqli_fetch_array($result);
+				$balance = $resultRow[1];
+			if($balance < 0){
+				echo error_without_field("Your account has been suspended. Please contact librarian for further information!");
+			} else {
+					$_SESSION['type'] = "member";
+					$_SESSION['id'] = $resultRow[0];
+					$_SESSION['username'] = $_POST['m_user'];
+					header('Location: home.php');
+				}
 			}
 		}
 	?>
